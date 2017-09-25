3cffcbb044fe6bf2d8737a42e1368d335a7101f2
Purdy <374266@flourbluffschools.net>
2017-09-25 07:44:09 -0500

    changed formatting

--- format_demo.py
+++ format_demo.py
@@ -5,9 +5,13 @@ print('Well, {} I have a few more questions for you...'.format(name))
 
 print()
 
-num_classes = input('How many classes are you taking this year? ')
-print("Wow {}! {} is a LOT of classes.  I bet you're busy!".format(name,num_classes))
+print('This line of text ',end='')
+print('and this line of text ',end='')
+print('appear on the same line!')
 
 print()
 
-print("Don't worry {}, I took {} classes one year and look at how I turned out!".format(name,num_classes))
+num_classes = input('How many classes are you taking this year? ')
+print("Wow {}! {} is a LOT of classes.  I bet you're busy!".format(name,num_classes))
+print("Don't worry {}, I took {} classes ".format(name, num_classes),end='')
+print('one year and look at how I turned out!')
