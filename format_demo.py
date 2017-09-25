afffe5aaebf2414d876991da4b44bd5db05d6ee6
Chad Purdy <chpurdy@gmail.com>
2017-09-24 20:09:22 -0500

    added demo

---
+++ format_demo.py
@@ -0,0 +1,13 @@
+name = input('What is your name? ')
+
+print('Hello ' + name + ' I hope you had a nice weekend!')
+print('Well, {} I have a few more questions for you...'.format(name))
+
+print()
+
+num_classes = input('How many classes are you taking this year? ')
+print("Wow {}! {} is a LOT of classes.  I bet you're busy!".format(name,num_classes))
+
+print()
+
+print("Don't worry {}, I took {} classes one year and look at how I turned out!".format(name,num_classes))
