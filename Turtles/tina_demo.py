95921ce95d43b001f56116b86113992b98075191
Purdy <374266@flourbluffschools.net>
2017-10-05 07:49:32 -0500

    added functions

--- Turtles/tina_demo.py
+++ Turtles/tina_demo.py
@@ -1,5 +1,24 @@
 from tina import *
 
+def right():
+    left()
+    left()
+    left()
+    
+def turn_around():
+    left()
+    left()
 
+def fl():
+    forward()
+    left()
 
+def square():
+    fl()
+    fl()
+    fl()
+    fl()
+    
+   
+square()
 
