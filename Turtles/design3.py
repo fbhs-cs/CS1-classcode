37268af4c6f1ae60c018be008ad896799b7b633e
Chad Purdy <chpurdy@gmail.com>
2017-10-09 21:10:45 -0500

    added design 3

---
+++ Turtles/design3.py
@@ -0,0 +1,23 @@
+from turtle import *
+from random import choice, randint
+  
+def draw_circles():
+    ht()
+    color_list = ['red','blue','green','orange','yellow','black','white']
+    for i in range(1,21):
+        pu()
+        color(choice(color_list))  #random from list
+        #color(color_list[i % 7])  #in the order of the list
+        radius = 300 - 15*i
+        begin_fill()
+        goto(0,-radius)
+        pd()
+        circle(radius)
+        end_fill()
+
+    
+def main():
+    speed(0)
+    draw_circles()
+    
+main()
