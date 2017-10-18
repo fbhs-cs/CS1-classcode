bf3889a2aeb0157236977756756a2ebb9f2b1166
Chad Purdy <cpurdy@flourbluffschools.net>
2017-10-18 08:46:19 -0500

    class code

---
+++ Conditionals/if_demo1.py
@@ -0,0 +1,5 @@
+x = 7
+if x < 10:
+    print( "This line is only executed if x < 10." )
+    print( "And the same holds for this line." )
+print( "This line, however, is always executed." )
---
+++ Conditionals/logical_operators_demo1.py
@@ -0,0 +1,12 @@
+t = True
+f = False
+print( t and t )
+print( t and f )
+print( f and t )
+print( f and f )
+print( t or t )
+print( t or f )
+print( f or t )
+print( f or f )
+print( not t )
+print( not f )
