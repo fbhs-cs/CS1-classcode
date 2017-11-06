5694f6b0ec6e14e28f42525ebdf90b9f64894b99
Chad Purdy <cpurdy@flourbluffschools.net>
2017-11-06 08:31:34 -0600

    class code

---
+++ Functions/fruitful_function_class.py
@@ -0,0 +1,23 @@
+def area(radius):
+    '''returns the area of a circle with radius
+    '''
+    return 0.0
+
+
+def absolute_value(x):
+    '''returns the absolute value of x
+    '''
+    
+
+def compare(x, y):
+    '''returns 1 if x is greater than  y
+       returns 0 if x is equal to y
+       returns -1 if x is less than y
+    '''
+    return 0
+
+
+
+                   
+
+
